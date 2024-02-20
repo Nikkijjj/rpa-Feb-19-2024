aa1bd667af8c7f979624e7bfbf4bdc6f4d943b32
Nikkijjj <129257461+Nikkijjj@users.noreply.github.com>
2024-02-20 16:38:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,8 @@ def image_gpt():
 
 @app.route("/end", methods=["GET","POST"])
 def end():
-  global first_time=1
+  global first_time = 1
   return (render_template("end.html"))
-
 
 if __name__ == "__main__":
   app.run()
