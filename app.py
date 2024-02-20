4ae2d8c1a03440ed51c902e8f4d57f24496801fb
Nikkijjj <129257461+Nikkijjj@users.noreply.github.com>
2024-02-20 15:01:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def main():
   r = request.form.get("r")
   return (render_template("main.html", r=r))
 
-@app.route("/imagegpt", methods=["GET","POST"])
+@app.route("/image_gpt", methods=["GET","POST"])
 def main():
-  return (render_template("imagegpt.html"))
+  return (render_template("image_gpt.html"))
 
 if __name__ == "__main__":
   app.run()
