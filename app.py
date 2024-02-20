637338e4724e9b498bc4634c9fa6d2e395af849c
Nikkijjj <129257461+Nikkijjj@users.noreply.github.com>
2024-02-20 14:22:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# front part
 from flask import Flask,request,render_template
 
 app = Flask(__name__)
@@ -6,6 +5,11 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
   return (render_template("index.html"))
+
+@app.route("/main", method=["GET","POST"])
+def main():
+  name = request.form.get("name")
+  return (render_template("main.html"))
 
 if __name__ == "__main__":
   app.run()
